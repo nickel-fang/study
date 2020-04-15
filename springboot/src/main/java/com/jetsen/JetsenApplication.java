1eb2b1c36053ba57b685527c8eec6e722aa49a71
nickel <nickel.fang79@gmail.com>
2020-04-15 22:31:38 +0800

    modify gitignore

---
+++ springboot/src/main/java/com/jetsen/JetsenApplication.java
@@ -0,0 +1,38 @@
+package com.jetsen;
+
+import com.jetsen.service.StorageService;
+import io.netty.util.internal.NoOpTypeParameterMatcher;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * @author NICKEL
+ */
+@SpringBootApplication
+@EnableScheduling
+@EnableWebSecurity
+@EnableCaching
+@EnableDubbo
+public class JetsenApplication{
+    public static void main(String[] args) {
+        SpringApplication.run(JetsenApplication.class, args);
+    }
+
+}
